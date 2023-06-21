6fbe15218e13f3f2ef9c7f1114481ea2fec8960b
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-06-21 07:46:21 +0000

    [Finish] 4-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
   get 'lists/index'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  # .../lists/1 や.../lists/3に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
