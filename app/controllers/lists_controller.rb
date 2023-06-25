8949b18851e86bec7609bc710c13e16e7207bdd2
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-06-25 05:58:56 +0000

    [Finish] 4-10

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,6 +9,8 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list.id)
     else
+      # @lists = List.all
+      # render :index
       render :new
     end
   end
